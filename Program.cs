97ed57f98e0494dcd58ef7b3b1df61b5a6f29757
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-25 21:40:54 +0900

    Add
    Add

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Wood wood = new Wood();
+        wood.Efect();
+        Iron iron = new Iron();
+        iron.Efect();
+        Grass grass= new Grass();   
+        grass.Efect();
+    }
+}
+
+public interface IDamager
+{
+    void Efect();
+
+}
+public class Wood : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("duc lo khi dan ban");
+
+    }
+}
+public class Iron : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("toe lua khi dan ban");
+
+    }
+}
+public class Grass : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("tao bun dat khi dan ban");
+
+    }
+}
